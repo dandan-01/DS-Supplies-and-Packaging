84a4c0de296cbdd9eedf139152f72d32b8c6675c
Dani <sanchezdanilyn@gmail.com>
2023-07-26 17:19:05 -0500

    Finished top header

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ function shorten200($content, $maxLength = 200) {
         return $excerpt;
     } else {
         return $content;
-    }
+    } //
 }
 
 ?>
@@ -44,51 +44,68 @@ function shorten200($content, $maxLength = 200) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="main.css" type="text/css">
-    <title>DS Supplies and Packaging</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Boxed N Loaded</title>
 </head>
 <body>
     <!-- Remember that alternative syntax is good and html inside php is bad -->
-    <div id="header">
+    <header>
         <div id="logotitle">
             <a href="index.php">
                 <img src="imgs/box.jpg" alt="Box logo">
             </a>
 
             <a href="index.php">
-                <h1>DS Supplies and Packaging</h1>
+                <h1>Boxed N Loaded</h1>
+                <br>
+                <h4><i>"Expertly Packed, Safely Delivered to You!"</i></h4>
             </a>
         </div>
 
-        <div id="nav">
-            <a href="index.php">Home</a>
-            <a href="new_post.php">New Post</a>
+        <div class="searchNav">
+            <input type="search" name="search" id="search" placeholder="Search...">
+            <button type="submit"><i class="fa fa-search"></i></button>
         </div>
-    </div>
 
-    <h2>Showing Recent Blog Posts</h2>
+        <nav id="topright">
+            <button>Register</button>
+            <a href="index.php">Log In</a>
+            <a href="new_post.php">Cart</a>
+        </nav>
+    </header>
 
-    <div id="blogs">
-    <ul>
-        <?php foreach ($statement as $blogpost): ?>
-        <li>
-            <div class="blog-header">
-                <h2><a href="post.php?id=<?= $blogpost['id']; ?>"><?= $blogpost['title']; ?></a></h2>
-                <a href="edit.php?id=<?= $blogpost['id']; ?>">edit</a>
-            </div>
+    <nav>
+            <h2>Boxes</h2>
+            <h2>Bags</h2>
+            <h2>Supplies</h2>
+    </nav>
 
-            <div class="blog-timestamp">
-                <?= date("F d, Y, g:i a", strtotime($blogpost['timestamp'])); ?>
-            </div>
+    <section>
+        <h2>Showing Recent Blog Posts</h2>
 
-            <div class="blog-content">
-                <?= shorten200($blogpost['content']); ?>
-                <?php if (strlen($blogpost['content']) > 200): ?>
-                        <a href="post.php?id=<?= $blogpost['id']; ?>">Read Full Post</a>
-                <?php endif; ?>
-            </div>
-        </li>
-        <?php endforeach; ?>
-    </ul>
+        <div id="blogs">
+        <ul>
+            <?php foreach ($statement as $blogpost): ?>
+            <li>
+                <div class="blog-header">
+                    <h2><a href="post.php?id=<?= $blogpost['id']; ?>"><?= $blogpost['title']; ?></a></h2>
+                    <a href="edit.php?id=<?= $blogpost['id']; ?>">edit</a>
+                </div>
+
+                <div class="blog-timestamp">
+                    <?= date("F d, Y, g:i a", strtotime($blogpost['timestamp'])); ?>
+                </div>
+
+                <div class="blog-content">
+                    <?= shorten200($blogpost['content']); ?>
+                    <?php if (strlen($blogpost['content']) > 200): ?>
+                            <a href="post.php?id=<?= $blogpost['id']; ?>">Read Full Post</a>
+                    <?php endif; ?>
+                </div>
+            </li>
+            <?php endforeach; ?>
+        </ul>
+    </section>
 </div>
     
 </body>
